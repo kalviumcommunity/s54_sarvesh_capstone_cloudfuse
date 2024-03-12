4c663c926f0a4dd8524af136c2f26e5edf1cc2ec
Sarvesh <sarveshjaiofficial@gmail.com>
2024-03-12 11:54:43 +0530

    done shadcn setup

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
