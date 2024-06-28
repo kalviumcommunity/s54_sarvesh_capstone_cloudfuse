444a0c0e2be92a7280368cbc4fd9a4a699f5c6c9
aayushwrld <aayush.arora@kalvium.community>
2024-06-28 22:14:40 +0530

    Basic Navbar Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
-import NavBar from "@/components/NavBar";
+import NavBar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
